e0b6abc9a5d87404cf0a0ab6710375730d269ee2
ybarra <joe@thinkbean.com>
2016-09-10 10:26:30 -0400

    initial commit

---
+++ js/App.js
@@ -0,0 +1,19 @@
+var div = React.DOM.div
+var h1 = React.DOM.h1
+
+var MyTitle = React.createClass({
+    render () {
+        return (
+            div(null,
+                h1(null, 'Check out')
+            )
+        )
+    }
+})
+
+var MyFirstComponent = (
+    div(null,
+    React.createElement(MyTitle, null))
+)
+
+ReactDOM.render(MyFirstComponent, document.getElementById('app'))

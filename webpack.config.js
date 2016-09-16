d4d0c161789d7e5be91cf84aaf716abf4315a077
Joe Ybarra <joe@thinkbean.com>
2016-09-16 01:08:07 -0400

    SASS compile via webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,37 @@
 const path = require('path')
+const autoprefixer = require('autoprefixer')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const sassLoaders = [
+  'css-loader',
+  'postcss-loader',
+  'sass-loader?indentedSyntax=sass&includePaths[]=' + path.resolve(__dirname, './assets')
+]
 
 module.exports = {
   context: __dirname,
-  entry: './js/App.js',
+  entry: './assets/js/App.jsx',
   output: {
     path: path.join(__dirname, '/public'),
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['', '.js', '.jsx', '.json']
+    extensions: ['', '.js', '.jsx', '.json', '.scss'],
+    root: [path.join(__dirname, './assets')]
   },
   stats: {
     colors: true,
     reasons: true,
     chunks: false
   },
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ],
+  postcss: [
+    autoprefixer({
+      browsers: ['last 2 versions']
+    })
+  ],
   module: {
     loaders: [
       {
@@ -25,6 +42,10 @@ module.exports = {
           cacheDirectory: true,
           presets: ['react', 'es2015']
         }
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader', sassLoaders.join('!'))
       }
     ]
   }
